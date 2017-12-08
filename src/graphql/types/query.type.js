0be66f8d5b88f5e54057d273e16b820f47d00fe0
tvtienit <tvtienit@gmail.com>
2017-12-08 15:01:16 +0700

    Fix required field for extracting token

--- src/graphql/types/query.type.js
+++ src/graphql/types/query.type.js
@@ -1,6 +1,6 @@
 exports.query = `
     type Query {
-        extract(token: String): Auth,
+        extract(token: String!): Auth,
         users: [User],
         firstByUsername(username: String!): User,
         categoriesByCity(cgrId: String, city: String!, page: Int, limit: Int): [Category],
