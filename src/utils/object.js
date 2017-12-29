6b8b2a10d53ad4e865613ecfe35108d6780f1b04
tvtienit <tvtienit@gmail.com>
2017-12-29 19:36:01 +0700

    Remove object.js

--- src/utils/object.js
+++
@@ -1,9 +0,0 @@
-exports.cleanObject = (obj) => {
-    var propNames = Object.getOwnPropertyNames(obj);
-    for (var i = 0; i < propNames.length; i++) {
-      var propName = propNames[i];
-      if (obj[propName] === null || obj[propName] === undefined) {
-        delete obj[propName];
-      }
-    }
-  }
